33ccf5b84618ba6f1fc57ae1a1d3dfbf8bee346d
andy <33115218+tianhongbao@users.noreply.github.com>
2020-04-20 15:28:01 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "crow_all.h"
+int main()
+{
+    crow::SimpleApp app;
+
+    CROW_ROUTE(app, "/")([](){
+        return "Hello world";
+    });
+
+    app.port(8082).multithreaded().run();
+}
